3ade9c7e18b29706e18663af7c1ffb4c4e6ffa68
Sam Frank <rust@0xio.org>
2014-01-29 16:21:41 -0600

    cleaned up for new branch

--- include/Component.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMPONENT_H_INCLUDED
-#define COMPONENT_H_INCLUDED
-
-//	raw data that is attached to an entity, gets controlled by systems
-
-class Component {
-};
-
-#endif // COMPONENT_H_INCLUDED
